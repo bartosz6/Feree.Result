028b8363748baafc19b7343bf3723aa9a95e8ba4
Bartosz Kowalczyk <kontakt@feree.pl>
2017-12-28 17:17:44 +0100

    async binding

---
+++ src/Feree.ResultType/Feree.ResultType.Core/AsyncBinding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Feree.ResultType.Core
+{
+    public static class AsyncBinding
+    {
+        public static async Task<IResult<TOut>> BindAsync<TIn, TOut>(this IResult<TIn> source, Func<Task<IResult<TOut>>> next)
+        {
+            switch (source)
+            {
+                case Success<TIn> _:
+                    return await next();
+                case Failure<TIn> failure:
+                    return new Failure<TOut>(failure.Error);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+        
+        public static async Task<IResult<TOut>> BindAsync<TIn, TOut>(this Task<IResult<TIn>> source, Func<Task<IResult<TOut>>> next) => 
+            await (await source).BindAsync(async () => await next());
+
+        public static async Task<IResult<TOut>> BindAsync<TIn, TOut>(this Task<IResult<TIn>> source, Func<IResult<TOut>> next) => 
+            (await source).Bind(next);
+
+        public static Task<IResult<TOut>> BindAsync<TIn, TOut>(this IResult<TIn> source, Func<TIn, Task<IResult<TOut>>> next) => 
+            source.BindAsync(() => next(((Success<TIn>) source).Payload));
+        
+        public static Task<IResult<TOut>> BindAsync<TIn, TOut>(this Task<IResult<TIn>> source, Func<TIn, Task<IResult<TOut>>> next) => 
+            source.BindAsync(async () => await next(((Success<TIn>) await source).Payload));
+        
+        public static Task<IResult<TOut>> BindAsync<TIn, TOut>(this Task<IResult<TIn>> source, Func<TIn, IResult<TOut>> next) => 
+            source.BindAsync(async () => next(((Success<TIn>) await source).Payload));
+    }
+}
--- src/Feree.ResultType/Feree.ResultType.Core/ResultFactory.cs
+++ src/Feree.ResultType/Feree.ResultType.Core/ResultFactory.cs
@@ -1,4 +1,6 @@
-﻿namespace Feree.ResultType.Core
+﻿using System.Threading.Tasks;
+
+namespace Feree.ResultType.Core
 {
     public static class ResultFactory
     {
@@ -9,5 +11,12 @@
         public static IResult<T> CreateFailure<T>(string message) => new Failure<T>(new Error(message));
         public static IResult CreateFailure(IError error) => new Failure(error);
         public static IResult CreateFailure(string message) => new Failure(new Error(message));
+        
+        public static async Task<IResult<T>> CreateSuccessAsync<T>(Task<T> payload) => new Success<T>(await payload);
+        
+        public static async Task<IResult<T>> CreateFailure<T>(Task<IError> error) => new Failure<T>(await error);
+        public static async Task<IResult<T>> CreateFailure<T>(Task<string> message) => new Failure<T>(new Error(await message));
+        public static async Task<IResult> CreateFailure(Task<IError> error) => new Failure(await error);
+        public static async Task<IResult> CreateFailure(Task<string> message) => new Failure(new Error(await message));
     }
 }
