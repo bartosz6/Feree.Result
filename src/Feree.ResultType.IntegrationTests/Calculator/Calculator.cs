3fbcf2897d31edc801cf93d69012a5b0c0fc8a42
Bartosz Kowalczyk <kontakt@feree.pl>
2018-01-05 20:16:59 +0100

    circle ci

--- src/Feree.ResultType.IntegrationTests/Calculator/Calculator.cs
+++ src/Feree.ResultType.IntegrationTests/Calculator/Calculator.cs
@@ -18,7 +18,6 @@ namespace Feree.ResultType.IntegrationTests.Calculator
         public IResult<double> Multiply(double multiplicand, double multiplier) =>
             ResultFactory.CreateSuccess(multiplicand * multiplier);
 
-        // ReSharper disable once CompareOfFloatsByEqualityOperator
         public IResult<double> Divide(double dividend, double divisor) => divisor == 0
             ? ResultFactory.CreateFailure<double>("division by zero")
             : ResultFactory.CreateSuccess(dividend / divisor);
