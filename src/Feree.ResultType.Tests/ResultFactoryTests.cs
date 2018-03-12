6ea334b9275b35dca69a1bc119a5d6a36ed9b423
Bartosz Kowalczyk <kontakt@feree.pl>
2018-03-12 19:57:50 +0100

    test cleanup

--- src/Feree.ResultType.Tests/ResultFactoryTests.cs
+++ src/Feree.ResultType.Tests/ResultFactoryTests.cs
@@ -2,6 +2,7 @@
 using Feree.ResultType.Errors;
 using Feree.ResultType.Factories;
 using Feree.ResultType.Results;
+using Feree.ResultType.Tests.Helpers;
 using Feree.ResultType.Unit;
 using NUnit.Framework;
 
@@ -10,41 +11,38 @@ namespace Feree.ResultType.Tests
     public class ResultFactoryTests
     {
         private const string CurrentFileName = "ResultFactoryTests.cs";
+        private const string Message = "an error occured";
 
         [Test]
         public void CreateFailure_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled()
         {
-            var message = "an error occured";
-            var failure = (Failure<Empty>) ResultFactory.CreateFailure(message);
+            var failure = ResultFactory.CreateFailure(Message).AsFailure();
             
-            AssertFieldsAreFilled(failure, message, 18, nameof(CreateFailure_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
+            AssertFieldsAreFilled(failure, Message, 19, nameof(CreateFailure_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
         }
         
         [Test]
         public void CreateFailureOfT_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled()
         {
-            var message = "an error occured";
-            var failure = (Failure<int>) ResultFactory.CreateFailure<int>(message);
+            var failure = ResultFactory.CreateFailure<int>(Message).AsFailure();
             
-            AssertFieldsAreFilled(failure, message, 27, nameof(CreateFailureOfT_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
+            AssertFieldsAreFilled(failure, Message, 27, nameof(CreateFailureOfT_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
         }
         
         [Test]
         public async Task CreateFailureAsync_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled()
         {
-            var message = "an error occured";
-            var failure = (Failure<Empty>) await ResultFactory.CreateFailure(Task.Factory.StartNew(() => message));
+            var failure = (await ResultFactory.CreateFailure(Task.Factory.StartNew(() => Message))).AsFailure();
             
-            AssertFieldsAreFilled(failure, message, 36, nameof(CreateFailureAsync_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
+            AssertFieldsAreFilled(failure, Message, 35, nameof(CreateFailureAsync_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
         }
         
         [Test]
         public async Task CreateFailureOfTAsync_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled()
         {
-            var message = "an error occured";
-            var failure = (Failure<int>) await ResultFactory.CreateFailure<int>(Task.Factory.StartNew(() => message));
+            var failure = (await ResultFactory.CreateFailure<int>(Task.Factory.StartNew(() => Message))).AsFailure();
             
-            AssertFieldsAreFilled(failure, message, 45, nameof(CreateFailureOfTAsync_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
+            AssertFieldsAreFilled(failure, Message, 43, nameof(CreateFailureOfTAsync_GivenMessage_ReturnsFailureThatContainsErrorWithFieldsFilled));
         }
 
         private void AssertFieldsAreFilled<T>(Failure<T> failure, string message, int line, string methodName)
