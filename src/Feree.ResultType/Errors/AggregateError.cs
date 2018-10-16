e624221c52790acb93013b136c92df20aa9e4b11
Bartosz <bartosz.k.gdz@gmail.com>
2018-10-16 14:22:10 +0200

    flatten (#2)

---
+++ src/Feree.ResultType/Errors/AggregateError.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Feree.ResultType.Results;
+
+namespace Feree.ResultType.Errors
+{
+    public struct AggregateError : IError
+    {
+        public AggregateError(IError[] innerErrors) => InnerErrors = innerErrors;
+
+        public IReadOnlyCollection<IError> InnerErrors { get; }
+        public string Message => "aggregate error, see InnerErrors for details";
+    }
+}
--- src/Feree.ResultType/Results/Result.cs
+++ src/Feree.ResultType/Results/Result.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Feree.ResultType.Errors;
+using Feree.ResultType.Factories;
 
 namespace Feree.ResultType.Results
 {
@@ -96,5 +100,31 @@ namespace Feree.ResultType.Results
         public static async Task<IResult<TNext>> BindAsync<TPrev, TNext>(
             this Task<IResult<TPrev>> prev, Func<TPrev, Task<IResult<TNext>>> next) =>
             await (await prev).BindAsync(next);
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static IResult<IEnumerable<T>> Flatten<T>(this IEnumerable<IResult<T>> results)
+        {
+            var resultList = results.ToList();
+            var errors = resultList.OfType<Failure<T>>().Select(failure => failure.Error).ToArray();
+            var payloads = resultList.OfType<Success<T>>().Select(success => success.Payload);
+            return errors.Any()
+                ? ResultFactory.CreateFailure<IEnumerable<T>>(new AggregateError(errors))
+                : ResultFactory.CreateSuccess(payloads);
+        }
+        
+        public static async Task<IResult<IEnumerable<T>>> Flatten<T>(this IEnumerable<Task<IResult<T>>> results)
+        {
+            var resultList = results.ToList();
+            
+            await Task.WhenAll(resultList);
+
+            var errors = resultList.Select(t => t.Result).OfType<Failure<T>>().Select(failure => failure.Error).ToArray();
+            var payloads = resultList.Select(t => t.Result).OfType<Success<T>>().Select(success => success.Payload);
+            return errors.Any()
+                ? ResultFactory.CreateFailure<IEnumerable<T>>(new AggregateError(errors))
+                : ResultFactory.CreateSuccess(payloads);
+        }
     }
 }
