2144ffda255a8b7f92a40c6e1934aa8574d5fb16
Bartosz <bartosz.k.gdz@gmail.com>
2019-02-25 10:33:39 +0100

    converter to unit

--- test/Feree.ResultType.UnitTests/ToUnitConvertersTests.cs
+++ test/Feree.ResultType.UnitTests/ToUnitConvertersTests.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Feree.ResultType.Converters;
 using Feree.ResultType.Factories;
 using Feree.ResultType.Results;
@@ -24,6 +25,26 @@ namespace Feree.ResultType.UnitTests
             var resultOfString = ResultFactory.CreateFailure<string>("sup mate");
 
             var resultOfUnit = resultOfString.ToUnit();
+
+            resultOfUnit.ShouldBeOfType(typeof(Failure<Unit>));
+        }
+        
+        [Fact]
+        public async Task GivenSuccessOfStringAsync_ReturnSuccessOfUnitAsync()
+        {
+            var resultOfString = Task.FromResult(ResultFactory.CreateSuccess("sup mate"));
+
+            var resultOfUnit = await resultOfString.ToUnitAsync();
+
+            resultOfUnit.ShouldBeOfType(typeof(Success<Unit>));
+        }
+        
+        [Fact]
+        public async Task GivenFailureOfStringAsync_ReturnsFailureOfUnitAsync()
+        {
+            var resultOfString = Task.FromResult(ResultFactory.CreateFailure<string>("sup mate"));
+
+            var resultOfUnit = await resultOfString.ToUnitAsync();
 
             resultOfUnit.ShouldBeOfType(typeof(Failure<Unit>));
         }
