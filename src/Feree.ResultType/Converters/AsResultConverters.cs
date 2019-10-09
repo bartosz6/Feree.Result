e3352a56dfb7e85bcc9edcd3cfd88d7e713c55da
Bartosz <bartosz.k.gdz@gmail.com>
2019-10-09 14:17:24 +0200

    Feature/asresultasync (#10)
    * asresultasync with valuetask
    
    * asresultasync with valuetask

--- src/Feree.ResultType/Converters/AsResultConverters.cs
+++ src/Feree.ResultType/Converters/AsResultConverters.cs
@@ -7,6 +7,8 @@ namespace Feree.ResultType.Converters
     public static class AsResultConverters
     {
         public static IResult<T> AsResult<T>(this T @object) => ResultFactory.CreateSuccess(@object);
+        public static ValueTask<IResult<T>> AsResultAsync<T>(this T @object) => 
+           new ValueTask<IResult<T>>(ResultFactory.CreateSuccess(@object));
 
         public static async Task<IResult<T>> AsResultAsync<T>(this Task<T> task) => ResultFactory.CreateSuccess(await task);
     }
