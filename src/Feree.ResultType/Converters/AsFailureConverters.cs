370f03a608ab6b1a42ad0e4b937d03b6620a74e7
Bartosz <bartosz.k.gdz@gmail.com>
2019-10-09 14:42:34 +0200

    added as failure extension on string

---
+++ src/Feree.ResultType/Converters/AsFailureConverters.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Feree.ResultType.Factories;
+using Feree.ResultType.Results;
+// ReSharper disable ExplicitCallerInfoArgument
+
+namespace Feree.ResultType.Converters
+{
+    public static class AsFailureConverters
+    {
+        [DebuggerStepThrough]
+        public static IResult<T> AsFailure<T>(this string errorMessage,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0) => 
+            ResultFactory.CreateFailure<T>(errorMessage, memberName, sourceFilePath, sourceLineNumber);
+        
+        [DebuggerStepThrough]
+        public static IResult<Unit> AsFailure(this string errorMessage,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0) => 
+            ResultFactory.CreateFailure(errorMessage, memberName, sourceFilePath, sourceLineNumber);
+        
+        [DebuggerStepThrough]
+        public static ValueTask<IResult<Unit>> AsFailureAsync(this string errorMessage,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0) => 
+        new ValueTask<IResult<Unit>>(errorMessage.AsFailure(memberName, sourceFilePath, sourceLineNumber));
+        
+        [DebuggerStepThrough]
+        public static ValueTask<IResult<T>> AsFailureAsync<T>(this string errorMessage,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0) => 
+        new ValueTask<IResult<T>>(errorMessage.AsFailure<T>(memberName, sourceFilePath, sourceLineNumber));
+    }
+}
