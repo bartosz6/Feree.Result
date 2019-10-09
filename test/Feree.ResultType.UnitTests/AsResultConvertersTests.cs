5d15850b3ca268be14c901c0ebfa9e51e95238e2
Bartosz <bartosz.k.gdz@gmail.com>
2019-10-09 14:16:01 +0200

    asresultasync with valuetask

--- test/Feree.ResultType.UnitTests/AsResultConvertersTests.cs
+++ test/Feree.ResultType.UnitTests/AsResultConvertersTests.cs
@@ -1,7 +1,19 @@
+using System.Threading.Tasks;
+using Feree.ResultType.Converters;
+using Shouldly;
+using Xunit;
+
 namespace Feree.ResultType.UnitTests
 {
     public class AsResultConvertersTests
     {
-        
+        [Fact]
+        public async Task AsResultAsync_usedOnPayload_returnsSuccess()
+        {
+            var result = await "test".AsResultAsync();
+            
+            result.ShouldBeSuccess();
+            result.Payload().ShouldBe("test");
+        }
     }
 }
